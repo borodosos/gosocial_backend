83637fd174f1bb60bf507cfa5052873996e902c9
Georgiy Malahov <g.malahov@tg.dunice.net>
2023-01-25 09:38:54 +0300

    db init (user and post migration, models)

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'second_name',
+        'image_profile',
         'email',
         'password',
     ];
