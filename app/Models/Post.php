75fe0eeb67b66253b007b7bbfac5f51952e5020c
Georgiy Malahov <g.malahov@tg.dunice.net>
2023-01-25 09:50:34 +0300

    relations between user and posts/ change models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'text',
+        'image',
+        'tag'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
