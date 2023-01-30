02794637fd653570df999b47991056c1f298074e
Georgiy Malahov <g.malahov@tg.dunice.net>
2023-01-30 12:11:19 +0300

    add route registration and set ttl token

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('posts', [PostController::class, 'index']);
+
+// === Auth ===
+Route::post('registration', [AuthController::class, 'registration']);
