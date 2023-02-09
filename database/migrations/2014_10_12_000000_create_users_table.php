88d943b936104b247a1316f08938b115755010ad
Georgiy Malahov <g.malahov@tg.dunice.net>
2023-02-09 14:12:27 +0300

    fix errors

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('second_name');
             $table->string('email')->unique();
-            $table->string('image_profile')->nullable()->default(0);
+            $table->string('image_profile')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
