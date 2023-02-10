2ec0e79555597e8052540eb3fedd73539060e8c5
Georgiy Malahov <g.malahov@tg.dunice.net>
2023-02-10 16:49:04 +0300

    install socialite/get info user

---
+++ app/Http/Controllers/GoogleController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->stateless()->redirect();
+    }
+
+    public function loginWithGoogle(Request $request)
+    {
+        $user = Socialite::driver('google')->stateless()->user();
+
+        return response()->json($user);
+    }
+}
