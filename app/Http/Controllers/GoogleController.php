b5a756216ba77533204aacb97a43e7e73da5be48
Georgiy Malahov <g.malahov@tg.dunice.net>
2023-02-13 15:39:05 +0300

    remake google auth

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -18,54 +18,34 @@ class GoogleController extends Controller
         return response()->json($url);
     }
 
-    public function loginWithGoogle(Request $request)
+    public function loginWithGoogle()
     {
         $google_user = Socialite::driver('google')->stateless()->user();
 
-        $founded_user = User::where('google_id', $google_user->id)->first();
-
-        if ($founded_user) {
-            $req = Request::create('/oauth/token', 'POST', [
-                'grant_type' => 'password',
-                'client_id' => config('passport.password_grant_client.id'),
-                'client_secret' => config('passport.password_grant_client.secret'),
-                'username' => $founded_user->email,
-                'password' => 'nopass',
-                'scope' => '',
-            ]);
-
-
-            $res =  app()->handle($req);
-            $responseBody =  json_decode($res->getContent());
-
-            return redirect("http://localhost:8080/")->withCookie('refreshToken', $responseBody->refresh_token);
-            // return redirect("http://localhost:8080");
-            // return redirect()->route('login', $founded_user);
-            // return response()->json($responseBody);
-        } else {
-
-            $new_user = User::create([
-                'first_name' => $google_user->given_name,
-                'second_name' => $google_user->family_name,
+        $created_user = User::updateOrCreate(
+            ['google_id' => $google_user->id],
+            [
+                'first_name' => $google_user->user['given_name'],
+                'second_name' => $google_user->user['family_name'],
                 'email' => $google_user->email,
                 'google_id' => $google_user->id,
                 'password' => Hash::make('nopass'),
-            ]);
+            ]
+        );
 
-            $req = Request::create('/oauth/token', 'POST', [
-                'grant_type' => 'password',
-                'client_id' => config('passport.password_grant_client.id'),
-                'client_secret' => config('passport.password_grant_client.secret'),
-                'username' => $new_user->email,
-                'password' => 'nopass',
-                'scope' => '',
-            ]);
+        $req = Request::create('/oauth/token', 'POST', [
+            'grant_type' => 'password',
+            'client_id' => config('passport.password_grant_client.id'),
+            'client_secret' => config('passport.password_grant_client.secret'),
+            'username' => $created_user->email,
+            'password' => 'nopass',
+            'scope' => '',
+        ]);
 
 
-            $res =  app()->handle($req);
-            $responseBody =  json_decode($res->getContent());
+        $res =  app()->handle($req);
+        $responseBody =  json_decode($res->getContent());
 
-            return redirect('http://localhost:8080/')->withCookie(cookie('refreshToken', $responseBody->refresh_token));
-        }
+        return redirect("http://localhost:8080/")->withCookie('refreshToken', $responseBody->refresh_token);
     }
 }
