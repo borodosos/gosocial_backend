7e384663b80bbdaa1c01bb1357733999ba9c5cc7
Georgiy Malahov <g.malahov@tg.dunice.net>
2023-03-02 17:59:12 +0300

    make testing events and channels

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Events\PlaygroundEvent;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\GoogleController;
@@ -24,6 +25,10 @@ Route::resource('users', UserController::class)->middleware('auth:api');
 Route::resource('posts', PostController::class)->middleware('auth:api');
 Route::resource('posts.comments', CommentController::class)->shallow()->middleware('auth:api');
 
+Route::get('playground', function () {
+    event(new PlaygroundEvent());
+    return response()->json('aboba');
+});
 
 
 // === Auth ===
